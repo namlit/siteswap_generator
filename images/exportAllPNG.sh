5be3cba63fd25c9668dee63aee296b2b0c8d3d22
Tilman Sinning <namlit.gninnis@gmail.com>
2019-10-14 23:39:06 +0200

    add icons to menu options and show as actions if room, closes #52

--- images/exportAllPNG.sh
+++ images/exportAllPNG.sh
@@ -5,3 +5,7 @@ inkscape appicon.svg --export-png ../app/src/main/res/mipmap-hdpi/appicon.png -w
 inkscape appicon.svg --export-png ../app/src/main/res/mipmap-xhdpi/appicon.png -w 96 -h 96
 inkscape appicon.svg --export-png ../app/src/main/res/mipmap-xxhdpi/appicon.png -w 144 -h 144
 inkscape appicon.svg --export-png ../app/src/main/res/mipmap-xxxhdpi/appicon.png -w 192 -h 192
+
+inkscape favorite_star_icon.svg --export-png ../app/src/main/res/drawable/favorite_star_icon.png -w 96 -h 96
+inkscape rotate_default_icon.svg --export-png ../app/src/main/res/drawable/rotate_default_icon.png -w 96 -h 96
+inkscape named_siteswaps_icon.svg --export-png ../app/src/main/res/drawable/named_siteswaps_icon.png -w 96 -h 96
