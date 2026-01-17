2f70484173b1a200330c9e9849626cbb4d4b51e6
Tilman Sinning <developer+git@tilman-sinning.de>
2026-01-17 12:45:04 +0100

    feat: Make app icon round

--- images/exportAllPNG.sh
+++ images/exportAllPNG.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
+# Regular app icons (legacy, for Android < 8.0)
 inkscape appicon.svg+xml --export-type=png --export-filename="../app/src/main/res/mipmap-mdpi/appicon.png" -w 48 -h 48
 inkscape appicon.svg+xml --export-type=png --export-filename="../app/src/main/res/mipmap-hdpi/appicon.png" -w 72 -h 72
 inkscape appicon.svg+xml --export-type=png --export-filename="../app/src/main/res/mipmap-xhdpi/appicon.png" -w 96 -h 96
 inkscape appicon.svg+xml --export-type=png --export-filename="../app/src/main/res/mipmap-xxhdpi/appicon.png" -w 144 -h 144
 inkscape appicon.svg+xml --export-type=png --export-filename="../app/src/main/res/mipmap-xxxhdpi/appicon.png" -w 192 -h 192
+
+# Adaptive icon foregrounds (for Android 8.0+)
+# These are 108dp x 108dp with the icon scaled to fill more space
+inkscape appicon.svg+xml --export-type=png --export-filename="../app/src/main/res/mipmap-mdpi/appicon_foreground.png" -w 108 -h 108
+inkscape appicon.svg+xml --export-type=png --export-filename="../app/src/main/res/mipmap-hdpi/appicon_foreground.png" -w 162 -h 162
+inkscape appicon.svg+xml --export-type=png --export-filename="../app/src/main/res/mipmap-xhdpi/appicon_foreground.png" -w 216 -h 216
+inkscape appicon.svg+xml --export-type=png --export-filename="../app/src/main/res/mipmap-xxhdpi/appicon_foreground.png" -w 324 -h 324
+inkscape appicon.svg+xml --export-type=png --export-filename="../app/src/main/res/mipmap-xxxhdpi/appicon_foreground.png" -w 432 -h 432
 
 #inkscape favorite_star_icon.svg   --export-type=png --export-filename="./app/src/main/res/drawable/favorite_star_icon.png" -w 96 -h 96
 #inkscape rotate_default_icon.svg  --export-type=png --export-filename="./app/src/main/res/drawable/rotate_default_icon.png" -w 96 -h 96
