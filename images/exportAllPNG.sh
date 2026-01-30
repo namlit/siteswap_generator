417fcc1f6896f3001952ebc5737a902cb082f998
Tilman Sinning <developer+git@tilman-sinning.de>
2026-01-30 18:09:47 +0100

    chore: scale appicon correctly

--- images/exportAllPNG.sh
+++ images/exportAllPNG.sh
@@ -9,11 +9,11 @@ inkscape appicon.svg+xml --export-type=png --export-filename="../app/src/main/re
 
 # Adaptive icon foregrounds (for Android 8.0+)
 # These are 108dp x 108dp with the icon scaled to fill more space
-inkscape appicon.svg+xml --export-type=png --export-filename="../app/src/main/res/mipmap-mdpi/appicon_foreground.png" -w 108 -h 108
-inkscape appicon.svg+xml --export-type=png --export-filename="../app/src/main/res/mipmap-hdpi/appicon_foreground.png" -w 162 -h 162
-inkscape appicon.svg+xml --export-type=png --export-filename="../app/src/main/res/mipmap-xhdpi/appicon_foreground.png" -w 216 -h 216
-inkscape appicon.svg+xml --export-type=png --export-filename="../app/src/main/res/mipmap-xxhdpi/appicon_foreground.png" -w 324 -h 324
-inkscape appicon.svg+xml --export-type=png --export-filename="../app/src/main/res/mipmap-xxxhdpi/appicon_foreground.png" -w 432 -h 432
+inkscape appicon-round.svg --export-type=png --export-filename="../app/src/main/res/mipmap-mdpi/appicon_foreground.png" -w 108 -h 108
+inkscape appicon-round.svg --export-type=png --export-filename="../app/src/main/res/mipmap-hdpi/appicon_foreground.png" -w 162 -h 162
+inkscape appicon-round.svg --export-type=png --export-filename="../app/src/main/res/mipmap-xhdpi/appicon_foreground.png" -w 216 -h 216
+inkscape appicon-round.svg --export-type=png --export-filename="../app/src/main/res/mipmap-xxhdpi/appicon_foreground.png" -w 324 -h 324
+inkscape appicon-round.svg --export-type=png --export-filename="../app/src/main/res/mipmap-xxxhdpi/appicon_foreground.png" -w 432 -h 432
 
 #inkscape favorite_star_icon.svg   --export-type=png --export-filename="./app/src/main/res/drawable/favorite_star_icon.png" -w 96 -h 96
 #inkscape rotate_default_icon.svg  --export-type=png --export-filename="./app/src/main/res/drawable/rotate_default_icon.png" -w 96 -h 96
